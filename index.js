036d15b63eecfe3a046034292da3f2a682a12a3f
Arpit <arpitshukla993@gmail.com>
2024-06-08 09:51:19 +0530

    Final commit

--- index.js
+++ index.js
@@ -1,22 +1,45 @@
 const express = require("express");
 const http = require("http");
 
-const socketio = require("socket.io");
+const connect = require("./config/database-config.js");
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
-
+const { Server } = require("socket.io");
+const io = new Server(server);
+const Chat = require('./models/chat.js')
 io.on("connection", (socket) => {
   console.log("a user connected", socket.id);
-  socket.on("msg_send", (data) => {
+
+  socket.on("join_room", (data) => {
+    socket.join(data.roomid);
+  });
+
+  socket.on("msg_send",async (data) => {
     console.log(data);
-    io.emit("msg_recieved",data);
+    const chat = await Chat.create({
+      roomid:data.roomid,
+      content:data.msg,
+      user:data.username
+    })
+    io.to(data.roomid).emit("msg_recieved", data);
+  });
+});
+app.set("view engine", "ejs");
+app.use("/", express.static(__dirname + "/public"));
+app.get("/chat/:roomid",async(req, res) => {
+  const chats = await Chat.find({
+    roomid:req.params.roomid
+  }).select('content')
+  res.render("index", {
+    name: "STERNER",
+    id: req.params.roomid,
+    chats:chats
   });
 });
 
-app.use("/", express.static(__dirname + "/public"));
-
-server.listen(3000, () => {
+server.listen(3000, async () => {
   console.log("Server Starteds");
+  await connect();
+  console.log("mongo connected");
 });
